228eb02ac3799170ab7c1bc14b76bce4cf6ad819
Tyler Hampton <tylerhampton@my.unt.edu>
2023-10-30 06:31:26 -0500

    Implemented Color Themes to Settings Page

--- StudyBug/StudyBug/Views/Settings.xaml.cs
+++ StudyBug/StudyBug/Views/Settings.xaml.cs
@@ -16,5 +16,80 @@ namespace StudyBug.Views
 		{
 			InitializeComponent ();
 		}
-	}
+
+        private void Theme1Button_CheckedChanged_1(object sender, CheckedChangedEventArgs e)
+        {
+			if (e.Value == true) {
+				Theme2Button.IsChecked = false; 
+				Theme3Button.IsChecked = false;
+				Theme4Button.IsChecked = false;
+
+                App.Current.Resources["Primary"] = App.Current.Resources["Theme1Background"];
+				App.Current.Resources["PageBackgroundColor"] = App.Current.Resources["Theme1Background"];
+                App.Current.Resources["CourseCardColor"] = App.Current.Resources["Theme1Primary"];
+                App.Current.Resources["mainButtonColor"] = App.Current.Resources["Theme1Primary"];
+                App.Current.Resources["ButtonTextColor"] = App.Current.Resources["Theme1Text"];
+                App.Current.Resources["TextColor"] = App.Current.Resources["Theme1Text"];
+                App.Current.Resources["TabBarColor"] = App.Current.Resources["Theme1Background"];
+                App.Current.Resources["ProgressBarColor"] = App.Current.Resources["Theme1TextDetail"];
+            }
+        }
+
+        private void Theme2Button_CheckedChanged(object sender, CheckedChangedEventArgs e)
+        {
+            if (e.Value == true)
+            {
+                Theme1Button.IsChecked = false;
+                Theme3Button.IsChecked = false;
+                Theme4Button.IsChecked = false;
+
+                App.Current.Resources["Primary"] = App.Current.Resources["Theme2Background"];
+                App.Current.Resources["PageBackgroundColor"] = App.Current.Resources["Theme2Background"];
+                App.Current.Resources["CourseCardColor"] = App.Current.Resources["Theme2Primary"];
+                App.Current.Resources["mainButtonColor"] = App.Current.Resources["Theme2Primary"];
+                App.Current.Resources["ButtonTextColor"] = App.Current.Resources["Theme2Text"];
+                App.Current.Resources["TextColor"] = App.Current.Resources["Theme2Text"];
+                App.Current.Resources["TabBarColor"] = App.Current.Resources["Theme2Background"];
+                App.Current.Resources["ProgressBarColor"] = App.Current.Resources["Theme2TextDetail"];
+            }
+        }
+
+        private void Theme3Button_CheckedChanged(object sender, CheckedChangedEventArgs e)
+        {
+            if (e.Value == true)
+            {
+                Theme1Button.IsChecked = false;
+                Theme2Button.IsChecked = false;
+                Theme4Button.IsChecked = false;
+
+                App.Current.Resources["Primary"] = App.Current.Resources["Theme3Background"];
+                App.Current.Resources["PageBackgroundColor"] = App.Current.Resources["Theme3Background"];
+                App.Current.Resources["CourseCardColor"] = App.Current.Resources["Theme3Primary"];
+                App.Current.Resources["mainButtonColor"] = App.Current.Resources["Theme3Primary"];
+                App.Current.Resources["ButtonTextColor"] = App.Current.Resources["Theme3Text"];
+                App.Current.Resources["TextColor"] = App.Current.Resources["Theme3Text"];
+                App.Current.Resources["TabBarColor"] = App.Current.Resources["Theme3Background"];
+                App.Current.Resources["ProgressBarColor"] = App.Current.Resources["Theme3TextDetail"];
+            }
+        }
+
+        private void Theme4Button_CheckedChanged(object sender, CheckedChangedEventArgs e)
+        {
+            if (e.Value == true)
+            {
+                Theme1Button.IsChecked = false;
+                Theme2Button.IsChecked = false;
+                Theme3Button.IsChecked = false;
+
+                App.Current.Resources["Primary"] = App.Current.Resources["Theme4Background"];
+                App.Current.Resources["PageBackgroundColor"] = App.Current.Resources["Theme4Background"];
+                App.Current.Resources["CourseCardColor"] = App.Current.Resources["Theme4Primary"];
+                App.Current.Resources["mainButtonColor"] = App.Current.Resources["Theme4Primary"];
+                App.Current.Resources["ButtonTextColor"] = App.Current.Resources["Theme4Text"];
+                App.Current.Resources["TextColor"] = App.Current.Resources["Theme4Text"];
+                App.Current.Resources["TabBarColor"] = App.Current.Resources["Theme4Background"];
+                App.Current.Resources["ProgressBarColor"] = App.Current.Resources["Theme4TextDetail"];
+            }
+        }
+    }
 }
