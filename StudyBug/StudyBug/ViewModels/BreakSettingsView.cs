7c699ec312bc344eb8eba44bb9b7a2860420fce9
jft0046 <145061185+jft0046@users.noreply.github.com>
2023-11-04 22:23:48 -0500

    Started implementing break feature (binding of break_frequency and break_length not working)

--- StudyBug/StudyBug/ViewModels/BreakSettingsView.cs
+++ StudyBug/StudyBug/ViewModels/BreakSettingsView.cs
@@ -1,6 +1,12 @@
-﻿using System;
+using StudyBug.Models;
+using StudyBug.Services;
+using StudyBug.Views;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Xml.Linq;
 using Xamarin.Forms;
 
 namespace StudyBug.ViewModels
@@ -10,5 +16,56 @@ namespace StudyBug.ViewModels
         public BreakSettingsView() 
         {
         }
+
+        string interval;
+
+        public string Break_Frequency
+        {
+            get { return interval; }
+            set
+            {
+                interval = value;
+                App.ActiveUser.break_frequency = int.Parse(interval);
+                OnPropertyChanged();
+            }
+        }
+
+        string length;
+
+        public string Break_Length
+        {
+            get { return length; }
+            set
+            {
+                length = value;
+                App.ActiveUser.break_frequency = int.Parse(interval);
+                OnPropertyChanged();
+            }
+        }
+
+        private Command save;
+
+        public ICommand Save
+        {
+            get
+            {
+                if (save == null)
+                {
+                    save = new Command(PerformSave);
+                }
+
+                return save;
+            }
+        }
+
+        private async void PerformSave()
+        {
+            if (interval != null)
+            {
+                var route = $"{nameof(Settings)}";
+                await Shell.Current.GoToAsync(route);
+            }
+        }
+
     }
 }
